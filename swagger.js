917e2b32a62b9b0dcc20557e4dd3d6f57dfd3ac4
Ulis11 <ulig11mx@gmail.com>
2025-10-26 03:00:21 -0600

    swagger port

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
         },
         servers: [
             { 
-                url: 'http://localhost:8000', description: 'Local server' 
+                url: 'http://localhost:5000', description: 'Local server' 
             }
         ],
         components: {
